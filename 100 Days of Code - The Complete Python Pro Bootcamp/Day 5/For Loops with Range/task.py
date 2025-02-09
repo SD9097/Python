8aba6b5a39d5a2b9c4bab264405a24c1af426ce1
SD9097 <ghanekarsubodh321@gmail.com>
2025-02-09 13:48:59 +0530

    Learn about for loop and made small project for password generator

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/For Loops with Range/task.py
@@ -0,0 +1,15 @@
+#indexing start from 0 so second number is not included
+# for number in  range(1,11):
+#     print(number)
+
+#the 3 mention is the step it adds to initial number
+# for i in range(1,11,3):
+#     print(i)
+
+total = 0
+
+for i in range(1,101):
+    total += i
+print(total)
+
+and
--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/Password Generator Project/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,37 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# total = nr_letters + nr_symbols + nr_numbers
+# letter_symbols = nr_letters + nr_symbols
+#
+# password = ""
+#
+# for i in range(1, total + 1):
+#     if i <= nr_letters:
+#         password += letters[random.randint(0, len(letters) - 1)]
+#     elif i <= letter_symbols:
+#         password += symbols[random.randint(0, len(numbers) - 1)]
+#     else:
+#         password += numbers[random.randint(0, len(symbols) - 1)]
+#
+# print("Your password is: " + password)
+
+#Hard level
+password_list = []
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(password)
 
